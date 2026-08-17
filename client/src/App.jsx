4d63beec7b7bc6ba0422d1c9af93b4d2750427e3
charan210 <reddycharan2580@gmail.com>
2026-08-17 14:11:34 +0530

    feat: add bug report submission form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,122 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority,setPriority] =useState("High");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
-      <div className="ticks"></div>
+  const handleSubmit = async (event) => {
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+  event.preventDefault();
+  if(!title.trim()){
+    setMessage("Title is required");
+    return;
+  }
+  if (!description.trim()) {
+  setMessage("Description is required.");
+  return;
 }
 
-export default App
+  try {
+
+      setLoading(true);
+    const response = await axios.post(
+      "http://localhost:5000/api/bugs",
+      {
+        title: title,
+        description: description,
+        priority: priority
+      }
+    );
+
+    setMessage(response.data.message);
+      setTitle("");
+     setDescription("");
+     setPriority("High");
+
+  } catch (error) {
+     setMessage("Failed to create bug");
+    console.log(error);
+
+  }
+  finally {
+  setLoading(false);
+}
+
+};
+  return (
+    <div>
+      <h1>🐞 BugTracker Pro</h1>
+
+      <h2>Report New Bug</h2>
+
+      <form onSubmit={handleSubmit}>
+
+        <div>
+          <label>Title</label>
+          <br />
+
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Description</label>
+          <br />
+
+          <textarea 
+            rows="4"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+           >
+          </textarea>
+        </div>
+
+        <br />
+
+        <div>
+          <label>Priority</label>
+          <br />
+
+          <select
+        value={priority}
+          onChange={(event) => setPriority(event.target.value)}
+         >
+            <option>High</option>
+            <option>Medium</option>
+            <option>Low</option>
+          </select>
+        </div>
+
+        <br />
+
+        <button type="submit" disabled={loading}>
+          {loading ? "Creaing Bug..... ": "submit Bug"}
+        </button>
+
+       <p>{message}</p>
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,32 @@
 const express=require("express");
+const cors = require("cors");
 const app=express();
+app.use(cors());
+app.use(express.json());
 const port=5000;
 
-app.get("/",(req,res)=>{
+
+app.get("/api/health",(req,res)=>{
     res.json({
-        "message":"Server is running by Chara reddy at port 5000",
-        "welcomeNote": " happy to weelcome you"
+         success: true,
+
+        message: "BugTracker Pro Backend is Running 🚀",
+        version: "1.0.0"
     })
 });
+
+
+app.post("/api/bugs", (req, res) => {
+
+    console.log(req.body);
+
+    res.json({
+        success: true,
+        message: "Bug received successfully!"
+    });
+
+});
+
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
